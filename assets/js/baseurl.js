c7ad21f6b9f7cb78fa29d9e78041d12e3670ae68
Mengniiu <www.wangchao4418@outlook.com>
2020-11-24 10:54:04 +0800

    注册登录完成

---
+++ assets/js/baseurl.js
@@ -0,0 +1,5 @@
+let baseurl='http://ajax.frontend.itheima.net';
+//ajax请求发送前触发的函数
+// $.ajaxPrefilter(function(res){
+//     res.url='http://ajax.frontend.itheima.net'+res.url
+// })
---
+++ assets/js/login.js
@@ -0,0 +1,64 @@
+$(function () {
+    $('#form-reg').on('submit', reg);
+    $('#form-login').on('submit', login);
+
+    $('#link-reg').on('click', function () {
+        $('.loginbox').hide();
+        $('.regbox').show();
+    })
+    $('#link-login').on('click', function () {
+        $('.loginbox').show();
+        $('.regbox').hide();
+    })
+
+    // 密码规则
+    layui.form.verify({
+        pw: [
+            /^[A-Z][\S]{6,12}$/
+            , '密码必须6到12位，首字母大写，且不能出现空格'
+        ],
+        rpw: function (rpw) {
+            let pw = $('#password').val();
+            if (rpw !== pw) {
+                return '两次密码输入不同，请重新输入'
+            }
+        }
+    })
+})
+
+function reg(e) {
+    e.preventDefault();
+    let fd = $(this).serialize();
+    $.ajax({
+        method: 'POST',
+        url: baseurl + '/api/reguser',
+        data: fd,
+        success(res) {
+            if (res.status != 0) return layui.layer.msg(res.message);
+            layui.layer.msg('注册成功快登录呀');
+            let username = $('#form-reg [name=username]').val();
+            $('#form-reg')[0].reset();
+            $('#link-login').click();
+            $('#form-login [name=username]').val(username);
+        }
+    })
+}
+function login(e) {
+    e.preventDefault();
+    let fd = $(this).serialize();
+    $.ajax({
+        method: 'POST',
+        url: baseurl + '/api/login',
+        data: fd,
+        success(res) {
+            if (res.status != 0) return layui.layer.msg(res.message);
+            layer.msg(res.message, {
+                icon: 6,
+                time: 1500 //1.5秒关闭（如果不配置，默认是3秒）
+            }, function () {
+                localStorage.setItem('mytoken', res.token);
+                location.href = '/index.html'
+            });
+        }
+    })
+}
